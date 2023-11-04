c1f198e631e00aed90b38767af059f16d6eab69d
Enes Gulakhmet <enes515254@gmail.com>
2023-11-04 09:26:29 +0200

    HF-26: Implemented Storage entity

---
+++ src/storages/entities/storage.py
@@ -0,0 +1,13 @@
+import datetime
+import uuid
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Storage:
+    link: str
+    user_id: uuid.UUID
+    primary: bool = False
+
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    created_at: datetime.datetime = field(default_factory=datetime.datetime.now)
