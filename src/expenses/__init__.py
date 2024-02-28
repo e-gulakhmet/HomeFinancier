29be87bdd5892fb9622751c228d001723a7c59cd
Enes Gulakhmet <enes515254@gmail.com>
2024-02-28 17:13:15 +0200

    HF-43: Minor fixes after review

--- src/expenses/__init__.py
+++ src/expenses/__init__.py
@@ -4,12 +4,16 @@ from .types import Amount, Category, ExpensesStorageLink, OwnerID
 from .usecases import ExpenseCreateInput, ExpenseCreateRepoInterface, ExpenseCreateUseCase
 
 __all__ = [
+    # entities
     "Expense",
+    # exceptions
     "UserShouldHavePrimaryStorageError",
+    # types
     "Amount",
     "Category",
     "ExpensesStorageLink",
     "OwnerID",
+    # usecases
     "ExpenseCreateInput",
     "ExpenseCreateRepoInterface",
     "ExpenseCreateUseCase",
