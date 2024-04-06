1428f15236e58988726950027383b941d1bbfee0
Enes Gulakhmet <enes515254@gmail.com>
2024-04-06 08:28:35 +0200

    main: Added reflection to `gRPCServer`

--- src/infrastructure/transport/grpc/grpc.py
+++ src/infrastructure/transport/grpc/grpc.py
@@ -1,4 +1,8 @@
+import logging
+
+import homefinancier.users.v1.users_pb2 as users_pb2  # noqa: PLR0402
 import homefinancier.users.v1.users_pb2_grpc as users_pb2_grpc  # noqa: PLR0402
+from grpc_reflection.v1alpha import reflection
 
 import grpc
 
@@ -9,11 +13,17 @@ class gRPCServer:  # noqa: N801 # gRPCServer looks better than GPRCServer (https
         self._server = grpc.aio.server()
         users_pb2_grpc.add_UsersServiceServicer_to_server(self._users_service, self._server)
 
-    async def start(self, host: str, port: int) -> None:
-        # Register services
+    def enable_reflection(self) -> None:
+        sevice_names = (
+            users_pb2.DESCRIPTOR.services_by_name["UsersService"].full_name,
+            reflection.SERVICE_NAME,
+        )
+        reflection.enable_server_reflection(sevice_names, self._server)
 
-        # Start server
+    async def start(self, host: str, port: int) -> None:
         address = f"{host}:{port}"
         self._server.add_insecure_port(address)
 
+        logging.info("Starting gRPC server on %s", address)
         await self._server.start()
+        await self._server.wait_for_termination()
